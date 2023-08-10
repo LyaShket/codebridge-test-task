18c09b941e1b611a0fdca5e9dfc6a375be9e6f4c
Dmytro Demyanenko <dmytro.demjanenko@gmail.com>
2023-08-10 11:11:53 +0300

    add styles according to figma prototype

--- src/app/pages/article/article.module.ts
+++ src/app/pages/article/article.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ArticleComponent } from './article.component';
 import { ArticleRoutingModule } from './article-routing.module';
 import { AppArticleViewModule } from '../../shared/app-article-view/app-article-view.module';
+import { AppLinkArrowModule } from '../../shared/app-link-arrow/app-link-arrow.module';
+import { MatCardModule } from '@angular/material/card';
 
 
 
@@ -14,6 +16,8 @@ import { AppArticleViewModule } from '../../shared/app-article-view/app-article-
     CommonModule,
     ArticleRoutingModule,
     AppArticleViewModule,
+    AppLinkArrowModule,
+    MatCardModule,
   ]
 })
 export class ArticleModule { }
--- src/app/shared/app-article-preview-card/app-article-preview-card.component.ts
+++ src/app/shared/app-article-preview-card/app-article-preview-card.component.ts
@@ -3,6 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-article-preview-card',
   templateUrl: './app-article-preview-card.component.html',
+  styleUrls: ['./app-article-preview-card.component.scss']
 })
 export class AppArticlePreviewCardComponent {
   @Input() created!: number | string;
